613a1af215b8ec6db06144a233dfd326de50e0e3
daniildn <dosindaniilm@gmail.com>
2022-01-02 13:49:40 +0300

    completed timer for terminal

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import time
 def countdown_timer(t):
     while t:
         mins, secs = divmod(t, 60)
-        timer = "{}:{}".format(mins, secs)
+        timer = "{:02d}:{:02d}".format(mins, secs)
         print(timer)
         time.sleep(1)
         t-=1
-countdown_timer(10)
+    print('finished')
+countdown_timer(3)
